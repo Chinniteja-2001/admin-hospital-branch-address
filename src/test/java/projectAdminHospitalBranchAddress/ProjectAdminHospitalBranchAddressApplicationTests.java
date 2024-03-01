57b6ccf0ea6144935413132db1e1b3344e12fd68
chinni teja <chinnitejapenta@gmail.com>
2024-03-01 11:40:28 +0530

    uploaded files

---
+++ src/test/java/projectAdminHospitalBranchAddress/ProjectAdminHospitalBranchAddressApplicationTests.java
@@ -0,0 +1,13 @@
+package projectAdminHospitalBranchAddress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAdminHospitalBranchAddressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
